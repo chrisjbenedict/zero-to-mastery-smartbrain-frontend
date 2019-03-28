ab40a91a9720a96bda69dd0869dcb19e2886e4a1
github username <github email address>
2019-03-28 13:28:32 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className="particles"
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
 }
 
 export default App;
+
+
+// app.models
+// .predict(
+// Clarifai.COLOR_MODEL,
+//     // URL
+//     "https://samples.clarifai.com/metro-north.jpg"
+// )
+// .then(function(response) {
+//     // do something with responseconsole.log(response);
+//     },
+//     function(err) {// there was an error}
+// );
